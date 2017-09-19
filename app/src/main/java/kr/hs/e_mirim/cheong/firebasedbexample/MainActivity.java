b28bdebf84765a17f6c15614e8815a8f508d58f8
imcheong <imcheong001@gmail.com>
2017-09-19 20:53:30 +0900

    #5 웹브라우져로 링크 띄우기 구현

--- app/src/main/java/kr/hs/e_mirim/cheong/firebasedbexample/MainActivity.java
+++ app/src/main/java/kr/hs/e_mirim/cheong/firebasedbexample/MainActivity.java
@@ -1,13 +1,15 @@
 package kr.hs.e_mirim.cheong.firebasedbexample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnContextClickListener{
     private TextView mNameTextView;
     private TextView mGithubTextView;
     private ImageView mProfilImageView;
@@ -22,7 +24,21 @@ public class MainActivity extends AppCompatActivity {
         mProfilImageView = (ImageView) findViewById(R.id.profile_image);
 
         ImageView mProfileImageView;
-        Picasso.with(this)
-                .load("http://i.imgur.com/DvpvklR.png");
-                .into(mProfilImageView);
+        Picasso.with(context).load("http://i.imgur.com/DvpvklR.png").into(mProfileImageView);
+        Picasso.with(context)
+                .load(url)
+                .resize(50, 50)
+                .centerCrop()
+                .into(imageView)
     }
+    public void onClick(View view){
+    switch (view.getId()){
+        case R.id.profile_github:
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            Url rul = Utl.parse(mGithubTextView.getText().toString());
+            Intent.setData(url);
+            startActivity(intent);
+            break;
+    }
+    }
+}
