4b2f2d3c050ff973b1b9095518fae5bcfeb78831
imcheong <imcheong001@gmail.com>
2017-09-19 19:58:11 +0900

    #2 뷰 참조 연결

--- app/src/main/java/kr/hs/e_mirim/cheong/firebasedbexample/MainActivity.java
+++ app/src/main/java/kr/hs/e_mirim/cheong/firebasedbexample/MainActivity.java
@@ -2,12 +2,19 @@ package kr.hs.e_mirim.cheong.firebasedbexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mNameTextView;
+    private TextView mGithubTextView;
+    private TextView mProfileImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mNameTextView = (TextView)findViewById(R.id.profile_name);
+        mGithubTextView = (TextView)findViewById(R.id.profile_github);
+        mProfileImageView = (TextView)findViewById(R.id.profile_image);
     }
 }
