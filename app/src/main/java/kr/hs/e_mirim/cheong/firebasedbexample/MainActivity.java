5b36207e1ffc7a693d03c4f6ea0d58b352144081
imcheong <imcheong001@gmail.com>
2017-09-19 20:43:45 +0900

    #4 picasso와의 만남

--- app/src/main/java/kr/hs/e_mirim/cheong/firebasedbexample/MainActivity.java
+++ app/src/main/java/kr/hs/e_mirim/cheong/firebasedbexample/MainActivity.java
@@ -1,20 +1,28 @@
 package kr.hs.e_mirim.cheong.firebasedbexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mNameTextView;
     private TextView mGithubTextView;
-    private TextView mProfileImageView;
+    private ImageView mProfilImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mNameTextView = (TextView)findViewById(R.id.profile_name);
-        mGithubTextView = (TextView)findViewById(R.id.profile_github);
-        mProfileImageView = (TextView)findViewById(R.id.profile_image);
+
+        mNameTextView = (TextView) findViewById(R.id.profile_name);
+        mGithubTextView = (TextView) findViewById(R.id.profile_github);
+        mProfilImageView = (ImageView) findViewById(R.id.profile_image);
+
+        ImageView mProfileImageView;
+        Picasso.with(this)
+                .load("http://i.imgur.com/DvpvklR.png");
+                .into(mProfilImageView);
     }
-}
